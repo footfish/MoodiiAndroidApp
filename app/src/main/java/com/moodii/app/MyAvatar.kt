2c03c2ac1a9679708470ac04059c62fd7c3cd0eb
Kevin Brennan <kevin.brennan@redsquared.com>
2018-02-17 10:35:09 +0000

    First commit

---
+++ app/src/main/java/com/moodii/app/MyAvatar.kt
@@ -0,0 +1,12 @@
+package com.moodii.app
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MyAvatar : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_avatar)
+    }
+}
