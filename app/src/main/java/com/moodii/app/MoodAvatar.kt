c227bd50dcbb55fd7c78a7559c639ae17d91b800
Kevin Brennan <kevin.brennan@redsquared.com>
2018-03-11 21:50:03 +0000

    added basic MoodAvatar activity

--- app/src/main/java/com/moodii/app/MoodAvatar.kt
+++ app/src/main/java/com/moodii/app/MoodAvatar.kt
@@ -1,48 +1,39 @@
 package com.moodii.app
 
-import android.content.Intent
 import android.graphics.Color
 import android.graphics.PorterDuff
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.AppCompatImageButton
 import android.support.v7.widget.AppCompatImageView
 import android.util.Log
 import android.view.Menu
-import android.view.View
-import android.widget.ImageButton
-import android.widget.ImageView
-import com.moodii.app.helpers.OnSwipeTouchListener
-import com.moodii.app.models.*
-import android.view.MenuInflater
 import android.view.MenuItem
+import com.moodii.app.models.*
+
+//val avatar = Avatar() //nts: temp, move to main
+private var selectedMood  = NEUTRAL
 
 
-val avatar = Avatar() //nts: temp, move to main
-private var selectedPart  = HEAD
-private var coloringMode  = false
-
-class EditAvatar : AppCompatActivity() {
+class MoodAvatar : AppCompatActivity() {
 
 
     //Add the action buttons to Navbar
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater = menuInflater
-        inflater.inflate(R.menu.edit_avatar, menu)
+        inflater.inflate(R.menu.mood_avatar, menu)
         return true
     }
 
     //Navbar actions
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
         R.id.action_save -> {
-            Log.d("Action","Save")
-            val intent = Intent(this, MoodAvatar::class.java)
-            startActivity(intent)
+            Log.d("Action", "Save")
             true
         }
 
         R.id.action_quit -> {
-            Log.d("Action","Quit")
+            Log.d("Action", "Quit")
             true
         }
        else -> { //action not recognised.
@@ -51,8 +42,8 @@ class EditAvatar : AppCompatActivity() {
     }
         override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_avatar)
-        setSupportActionBar(findViewById(R.id.my_toolbar))
+        setContentView(R.layout.activity_mood_avatar)
+        setSupportActionBar(findViewById(R.id.my_toolbar)) //add navbar
 
         val avatarViews = arrayOf<AppCompatImageView> (
                 findViewById(R.id.head),
@@ -65,29 +56,27 @@ class EditAvatar : AppCompatActivity() {
         )
 
         val buttonViews = arrayOf<AppCompatImageButton> (
-                findViewById(R.id.buttonHead),
-                findViewById(R.id.buttonHairTop),
-                findViewById(R.id.buttonHairBack),
-                findViewById(R.id.buttonEyes),
-                findViewById(R.id.buttonNose),
-                findViewById(R.id.buttonMouth),
-                findViewById(R.id.buttonEyebrows)
+                findViewById(R.id.buttonNeutral),
+                findViewById(R.id.buttonHappy),
+                findViewById(R.id.buttonSad),
+                findViewById(R.id.buttonScared),
+                findViewById(R.id.buttonAngry),
+                findViewById(R.id.buttonSurprised)
                 )
 
-        val iconToggleColorView= findViewById<AppCompatImageButton>(R.id.iconColorToggle)
         //init selected button
-        setButtonSelected(buttonViews, selectedPart,iconToggleColorView)
+        setButtonSelected(buttonViews, selectedMood)
+
         //set button listeners
-        for (i in HEAD until buttonViews.size) buttonViews[i].setOnClickListener { setButtonSelected(buttonViews,i,iconToggleColorView)}
+        for (i in NEUTRAL until buttonViews.size) buttonViews[i].setOnClickListener {
+            setButtonSelected(buttonViews,i)
+            renderMoodAvatar(avatarViews, i)
+        }
 
-        //render avatar parts
-        for (i in HEAD until avatarViews.size) renderPart(avatarViews[i],i)
-        //render avatar parts colors
-        renderPartColor(avatarViews, HEAD)
-        renderPartColor(avatarViews, HAIRTOP)
-        renderPartColor(avatarViews, EYEBROWS)
+        //render avatar
+        renderMoodAvatar(avatarViews, NEUTRAL)
 
-
+/*
         avatarViews[HEAD].setOnTouchListener(
                 object : OnSwipeTouchListener(this) {
                     override fun onSwipeRight() {
@@ -105,9 +94,9 @@ class EditAvatar : AppCompatActivity() {
                         } else {
                             when(selectedPart) {
                                 HEAD -> avatar.skinColor = AvatarFactory.getNextPartColor(avatar.skinColor, selectedPart)
-                                HAIRTOP -> avatar.hairColor = AvatarFactory.getNextPartColor(avatar.hairColor,selectedPart)
+                                HAIRTOP -> avatar.hairColor = AvatarFactory.getNextPartColor(avatar.hairColor, selectedPart)
                                 HAIRBACK -> avatar.hairColor = AvatarFactory.getNextPartColor(avatar.hairColor, selectedPart)
-                                EYEBROWS-> avatar.eyebrowsColor = AvatarFactory.getNextPartColor(avatar.eyebrowsColor, selectedPart)
+                                EYEBROWS -> avatar.eyebrowsColor = AvatarFactory.getNextPartColor(avatar.eyebrowsColor, selectedPart)
                             }
                             renderPartColor(avatarViews, selectedPart)
                         }
@@ -127,20 +116,22 @@ class EditAvatar : AppCompatActivity() {
                         } else {
                             when(selectedPart) {
                                 HEAD -> avatar.skinColor = AvatarFactory.getPrevPartColor(avatar.skinColor, selectedPart)
-                                HAIRTOP -> avatar.hairColor = AvatarFactory.getPrevPartColor(avatar.hairColor,selectedPart)
+                                HAIRTOP -> avatar.hairColor = AvatarFactory.getPrevPartColor(avatar.hairColor, selectedPart)
                                 HAIRBACK -> avatar.hairColor = AvatarFactory.getPrevPartColor(avatar.hairColor, selectedPart)
-                                EYEBROWS-> avatar.eyebrowsColor = AvatarFactory.getPrevPartColor(avatar.eyebrowsColor, selectedPart)
+                                EYEBROWS -> avatar.eyebrowsColor = AvatarFactory.getPrevPartColor(avatar.eyebrowsColor, selectedPart)
                             }
                             renderPartColor(avatarViews, selectedPart)
                         }
                     }
                 }
-        )
+        ) */
     }
 
-    //renders the avatars stored part of type 'partType' to the passed ImageView v.
-    private fun renderPart(v: ImageView, partType: Int) {
-            v.setImageResource(resources.getIdentifier(AvatarFactory.getResPart(avatar, partType), "drawable", packageName))
+    private fun renderMoodAvatar(avatarViews: Array<AppCompatImageView>, mood: Int) {
+        for ( partType in HEAD until avatarViews.size) {
+            avatarViews[partType].setImageResource(resources.getIdentifier(AvatarFactory.getResPart(avatar, partType, mood), "drawable", packageName))
+            renderPartColor(avatarViews,partType)
+        }
     }
 
     private fun renderPartColor(v: Array<AppCompatImageView>, partType: Int) {
@@ -163,35 +154,9 @@ class EditAvatar : AppCompatActivity() {
 
     }
 
-    private fun setButtonSelected(buttonViews: Array<AppCompatImageButton>, selectedButton: Int, iconToggleColorView: ImageView) {
-        selectedPart = selectedButton
-         iconToggleColorView.isSelected = coloringMode
-        for (i in HEAD until buttonViews.size) buttonViews[i].isSelected = (i == selectedButton) //highlights the selected button
-        when(selectedButton){
-            HEAD -> iconToggleColorView.visibility = View.VISIBLE
-            HAIRTOP -> iconToggleColorView.visibility=View.VISIBLE
-            HAIRBACK-> iconToggleColorView.visibility=View.VISIBLE
-            EYEBROWS -> iconToggleColorView.visibility=View.VISIBLE
-            MOUTH -> {
-                iconToggleColorView.visibility=View.GONE
-                coloringMode = false
-            }
-            EYES -> {
-                iconToggleColorView.visibility=View.GONE
-                coloringMode = false
-            }
-            NOSE -> {
-                iconToggleColorView.visibility=View.GONE
-                coloringMode = false
-            }
+    private fun setButtonSelected(buttonViews: Array<AppCompatImageButton>, selectedButton: Int) {
+        selectedMood = selectedButton
+        for (i in NEUTRAL until buttonViews.size) buttonViews[i].isSelected = (i == selectedButton) //highlights the selected button
         }
-
-    }
-
-    fun toggleColoringMode(v: View){
-        coloringMode = !coloringMode
-        v.isSelected = coloringMode
-    }
-
 
 }
--- app/src/main/java/com/moodii/app/models/AvatarFactory.kt
+++ app/src/main/java/com/moodii/app/models/AvatarFactory.kt
@@ -10,15 +10,22 @@ const val EYES = 3
 const val NOSE = 4
 const val MOUTH = 5
 const val EYEBROWS = 6
+const val NEUTRAL = 0
+const val HAPPY = 1
+const val SAD = 2
+const val SCARED = 3
+const val ANGRY = 4
+const val SURPRISED = 5
 private const val SKINC = 0
 private const val HAIRC = 1
+
 
 
 /**
  * Created by kevb on 25/02/2018.
  */
 object AvatarFactory { //set of avatar part id's (svg's)
-    val parts = arrayOf ( setOf("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24"), //HEAD
+    private val parts = arrayOf ( setOf("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24"), //HEAD
             setOf("1","2","3","4","5","6","7"), //HAIRTOP
             setOf("1","2","3","4","5","6","7"), //HAIRBACK
             setOf("1","2","3"), //EYES
@@ -27,22 +34,42 @@ object AvatarFactory { //set of avatar part id's (svg's)
             setOf("1","2","3","4") //EYEBROWS
             )
 
-    val partcolors = arrayOf (
+    private val partcolors = arrayOf (
             setOf("#f7e8a9","#ffd1ab","#ffd2d2","#d8e6b3","#ffd29d","#decbbc","#d2bcab", "#e3bb8d", "#cab26a", "#f9a824", "#dfa26f", "#d38d6f", "#090909", "#d5784d", "#b3723d","#a77b67", "#9a5b40", "#7c574f", "#854917", "#864d2c", "#723900", "#593b2b", "#42241a", "#3d0101" ), //SKINC
             setOf("#fffa84","#fbed66","#ffe12b","#e3dc3a","#e3dc3a","#c48647","#cc7227","#e76711","#a3672b","#a95712","#844b11","#733a00","#502f0c","#24150a","#090909","#cdc9d8","#dfe0df","#afbbe4", "#555755","#7c3030","#632929","#9f0000","#3a1212","#3d0101","#65245b","#811943","#e31d1d","#8524fc","#263ce3","#f94dac") //HAIRC
     )
 
-            // Returns the full resource name
-    fun getResPart(avatar: Avatar, partType:Int): String {
+    // Returns the full resource name
+    fun getResPart(avatar: Avatar, partType: Int, mood: Int = NEUTRAL): String {
         return( when (partType){
-            HEAD -> "head_" + avatar.headId
             HAIRTOP -> "hair_top_" + avatar.hairTopId
             HAIRBACK -> "hair_back_" + avatar.hairBackId
-            EYES -> "eyes_neutral_" + avatar.eyesId
+            EYES -> when(mood) {
+                HAPPY -> "eyes_happy_" + avatar.eyesId
+                SAD -> "eyes_sad_" + avatar.eyesId
+                SCARED -> "eyes_scared_" + avatar.eyesId
+                ANGRY -> "eyes_angry_" + avatar.eyesId
+                SURPRISED -> "eyes_surprised_" + avatar.eyesId
+                else ->  "eyes_neutral_" + avatar.eyesId
+            }
             NOSE -> "nose_" + avatar.noseId
-            MOUTH -> "mouth_neutral_" + avatar.mouthId
-            EYEBROWS -> "eyebrows_neutral_" + avatar.eyebrowsId
-            else -> ""
+            MOUTH -> when(mood) {
+                HAPPY -> "mouth_happy_" + avatar.mouthId
+                SAD -> "mouth_sad_" + avatar.mouthId
+                SCARED -> "mouth_scared_" + avatar.mouthId
+                ANGRY -> "mouth_angry_" + avatar.mouthId
+                SURPRISED -> "mouth_surprised_" + avatar.mouthId
+                else -> "mouth_neutral_" + avatar.mouthId
+            }
+            EYEBROWS -> when(mood) {
+                HAPPY ->   "eyebrows_happy_" + avatar.eyebrowsId
+                SAD ->   "eyebrows_sad_" + avatar.eyebrowsId
+                SCARED ->   "eyebrows_scared_" + avatar.eyebrowsId
+                ANGRY ->   "eyebrows_angry_" + avatar.eyebrowsId
+                SURPRISED ->   "eyebrows_surprised_" + avatar.eyebrowsId
+                else ->   "eyebrows_neutral_" + avatar.eyebrowsId
+            }
+            else -> "head_" + avatar.headId // HEAD/default
         })
     }
 
@@ -59,22 +86,20 @@ object AvatarFactory { //set of avatar part id's (svg's)
     //get's the previous part (of type partType) in the partcolors array in circular fashion
     fun getPrevPartColor(id: String, partType: Int): String {
         return (when (partType) {
-            HEAD -> getPrev(partcolors[SKINC], id)
             HAIRTOP -> getPrev(partcolors[HAIRC], id)
             HAIRBACK -> getPrev(partcolors[HAIRC], id)
             EYEBROWS -> getPrev(partcolors[HAIRC], id)
-            else -> ""
+            else -> getPrev(partcolors[SKINC], id)  // HEAD/default
         })
     }
 
     //get's the next part (of type partType) in the partscolors array in circular fashion
     fun getNextPartColor(id: String, partType: Int): String{
         return (when (partType) {
-            HEAD -> getNext(partcolors[SKINC], id)
             HAIRTOP -> getNext(partcolors[HAIRC], id)
             HAIRBACK -> getNext(partcolors[HAIRC], id)
             EYEBROWS -> getNext(partcolors[HAIRC], id)
-            else -> ""
+            else -> getNext(partcolors[SKINC], id)  // HEAD/default
         })
     }
 
