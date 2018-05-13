629fb06246c265974c37c133e75ef3fa73a5c2c8
Kevin Brennan <kevin.brennan@redsquared.com>
2018-05-13 21:36:43 +0100

    remove swipe hint on MoodAvatar activity

--- app/src/main/java/com/moodii/app/activities/MoodAvatar.kt
+++ app/src/main/java/com/moodii/app/activities/MoodAvatar.kt
@@ -173,7 +173,7 @@ class MoodAvatar : AppCompatActivity() {
         Log.w("MoodAvatar", "starting with mooder " + mooder.toString())
 
         //init buttons
-        selectedMood  = AvatarFactory.getMoodInt(mooder.mood.mood)
+         selectedMood  = AvatarFactory.getMoodInt(mooder.mood.mood)
         setButtonSelected(buttonViews, selectedMood)
 
         //set button listeners
